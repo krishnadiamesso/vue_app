973c29d10e45c53420889c640da99d85c2010a4f
Krishna Diamesso <krishnadiamesso@gmail.com>
2023-10-07 10:40:16 -0400

    Added Vur Components

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <Graph />
+  </div>
+</template>
+
+<script>
+import Graph from "~/components/Graph.vue";
+
+export default {
+  components: {Graph},
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ app/javascript/components/Graph.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+  <p>Real {{ count }}</p>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
